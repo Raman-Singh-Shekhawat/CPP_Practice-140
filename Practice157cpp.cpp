870f0ff795871f75982e1a08ac84853035427fce
Raman-Singh-Shekhawat <80272309+Raman-Singh-Shekhawat@users.noreply.github.com>
2022-10-10 23:17:35 +0530

    Add files via upload

---
+++ Practice157cpp.cpp
@@ -0,0 +1,36 @@
+// 158. Code : Find a node: For a given Binary Tree of type integer and a number X, find whether a node exists in the tree with data X or not.
+
+/************************************************************
+
+    Following is the Binary Tree node structure
+
+    template <typename T>
+    class BinaryTreeNode
+    {
+        public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data)
+        {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+bool isNodePresent(BinaryTreeNode<int> *root, int x)
+{
+    if (root == NULL)
+    {
+        return false;
+    }
+    if (root->data == x)
+    {
+        return true;
+    }
+    return isNodePresent(root->left, x) || isNodePresent(root->right, x);
+}
